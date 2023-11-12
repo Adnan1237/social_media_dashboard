f4080b7f14d7d493e6903a9c44c2de93513a0510
Adnan1237 <adnan1237@googlemail.com>
2023-11-12 12:49:41 +0000

    change for gunicorn

--- src/app.py
+++ src/app.py
@@ -3,13 +3,15 @@ from dash_bootstrap_components.themes import BOOTSTRAP
 from dashboard.components.layout import create_layout
 from dashboard.data.loader import load_insta_data
 
+app = Dash(__name__, external_stylesheets=[BOOTSTRAP])
+
 def main() -> None: 
     sc_data = load_insta_data()
-    app = Dash(external_stylesheets=[BOOTSTRAP]) 
-    server = app.server
+    # server = app.server
     app.layout = create_layout(app, sc_data)
-    app.run_server(debug=True)
+    # app.run_server(debug=True)
 
 
 if __name__ == "__main__":
     main()
+    app.run_server(debug=True)
