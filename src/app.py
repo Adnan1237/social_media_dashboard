b49ec73253100ca7b35cf0bdcf4b58c4513751e0
Adnan1237 <adnan1237@googlemail.com>
2023-11-12 12:56:49 +0000

    change

--- src/app.py
+++ src/app.py
@@ -3,15 +3,17 @@ from dash_bootstrap_components.themes import BOOTSTRAP
 from dashboard.components.layout import create_layout
 from dashboard.data.loader import load_insta_data
 
+# Define your Dash app
 app = Dash(__name__, external_stylesheets=[BOOTSTRAP])
 
-def main() -> None: 
-    sc_data = load_insta_data()
-    # server = app.server
-    app.layout = create_layout(app, sc_data)
-    # app.run_server(debug=True)
+# Define your server instance
+server = app.server
 
+# Create your Dash layout
+sc_data = load_insta_data()
+app.layout = create_layout(app, sc_data)
 
+# Entry point for running the app
 if __name__ == "__main__":
-    main()
     app.run_server(debug=True)
+
